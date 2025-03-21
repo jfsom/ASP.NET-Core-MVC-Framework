f21b1047c1f2e06808a3366700ad25d9ed7769d1
jfsom <vijay.jfsom@gmail.com>
2025-03-21 06:58:49 +0530

    How do we specify a custom controller and action method

--- FirstCoreMVCWebApplication/Program.cs
+++ FirstCoreMVCWebApplication/Program.cs
@@ -20,7 +20,10 @@ namespace FirstCoreMVCWebApplication
 
             // Map the default controller route (convention: {controller=Home}/{action=Index}/{id?})
             // This means if no specific route is provided, it will default to HomeController and Index action
-            app.MapDefaultControllerRoute();
+            app.MapControllerRoute(
+                name: "default", // Name of the route
+                pattern: "{controller=Home}/{action=Index}/{id?}" // URL pattern for the route
+            );
 
             // Run the application, which blocks the calling thread and starts listening for incoming HTTP requests
             app.Run();
